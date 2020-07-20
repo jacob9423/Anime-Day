3e9e0dd08bebd915d06bb3b19169d56694713bca
Jacob Barfield <jacob9423@gmail.com>
2020-07-19 18:53:05 -0700

    Finishing touches

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Anime-Day
-A Simple time converter for use for my friend and I to set up times to watch anime togethor 
+A Simple time converter for use for my friend and I to set up times to watch anime together. 
--- src/js/app.js
+++ src/js/app.js
@@ -1,84 +1,23 @@
+let PST = new spacetime();
+let EST = new spacetime();
+EST = EST.goto('America/New_York');
 
-let time = new Date();
+document.getElementById('txtPST').value = PST.time();
 
-let hPST = convertTime(time.getHours());
-let hEST = convertTime(time.getUTCHours() - 4);
-let AMorPMPST = getAMorPM(time.getHours());
-let AMorPMEST = getAMorPM(time.getUTCHours() - 4);
-var timeDif = (time.getUTCHours() - 4) - time.getHours();
+document.getElementById('txtEST').value = EST.time();
 
-let m = convertMins(time.getMinutes());
-    
-   
-
-
-
-document.getElementById('txtPST').value = hPST + ":" + m + AMorPMPST;
-document.getElementById('txtEST').value = hEST + ":" + m + AMorPMEST;
-
-console.log(timeDif);
+function toPST(){
+	EST = EST.time(document.getElementById('txtUserTime').value);
+	document.getElementById('txtEST').value = EST.time();
+	EST = EST.goto('America/Los_Angeles');
+	document.getElementById('txtPST').value = EST.time();
+	EST = EST.goto('America/New_York');
+}
 
 function toEST(){
-    let timePST = document.getElementById('txtUserTime').value;
-    let time = new Date();
-    let hPST = convertTime(time.getHours());
-    let hEST = convertTime(time.getUTCHours() - 4);
-    let AMorPMPST = getAMorPM(time.getHours());
-    let AMorPMEST = getAMorPM(time.getUTCHours() - 4);
-
-    let convertedTime = +timePST + +timeDif;
-    document.getElementById('txtEST').value = convertedTime + ":" + m + AMorPMEST;
-    document.getElementById('txtPST').value = timePST + ":" + m + AMorPMEST;
-    console.log(convertTime);
-}
-
-function convertMins(mins){
-    if(mins < 10){
-        mmins = "0" + mins;
-    }
-    return mins;
-}
-
-function convertTime(hour24Time){
-    let hour12Time;
-    if (hour24Time == 24){
-		hour12Time = 12;
-		AMorPM = "AM";
-		return hour12Time;
-	}else if (hour24Time >= 13 && hour24Time < 24){
-		hour12Time = hour24Time - 12;
-        AMorPM = "PM";
-		return hour12Time;
-	}else if (hour24Time == 12){
-		hour12Time = 12;
-		AMorPM = "PM";
-		return hour12Time;
-	}else if (hour24Time == 0){
-		hour12Time = 12;
-		AMorPM = "AM";
-		return hour12Time;
-	}else if (hour24Time > 0 || hour24Time <= 11){
-		hour12Time = hour24Time;
-		AMorPM = "AM";
-		return hour12Time;
-	}
-}
-
-function getAMorPM(hour24Time){
-    if (hour24Time == 24){
-		AMorPM = "AM";
-		return AMorPM;
-	}else if (hour24Time >= 13 && hour24Time < 24){
-        AMorPM = "PM";
-		return AMorPM;
-	}else if (hour24Time == 12){
-		AMorPM = "PM";
-		return AMorPM;
-	}else if (hour24Time == 0){
-		AMorPM = "AM";
-		return AMorPM;
-	}else if (hour24Time > 0 || hour24Time <= 11){
-		AMorPM = "AM";
-		return AMorPM;
-	}
+	PST = PST.time(document.getElementById('txtUserTime').value);
+	document.getElementById('txtPST').value = PST.time();
+	PST = PST.goto('America/New_York');
+	document.getElementById('txtEST').value = PST.time();
+	PST = PST.goto('America/Los_Angeles');
 }
